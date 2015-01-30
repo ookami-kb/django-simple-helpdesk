a0866ffff2a3a5919739b424d352568c2717d683
ookami.kb <ookami.kb@gmail.com>
2015-01-30 14:01:13 +0100

    fixed migrations

--- helpdesk/migrations/0001_initial.py
+++ helpdesk/migrations/0001_initial.py
@@ -11,44 +11,87 @@ class Migration(SchemaMigration):
         # Adding model 'Project'
         db.create_table(u'helpdesk_project', (
             ('machine_name', self.gf('django.db.models.fields.CharField')(max_length=64, primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('default_assignee', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
         ))
         db.send_create_signal(u'helpdesk', ['Project'])
+
+        # Adding model 'HelpdeskProfile'
+        db.create_table(u'helpdesk_helpdeskprofile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('signature', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal(u'helpdesk', ['HelpdeskProfile'])
 
         # Adding model 'State'
         db.create_table(u'helpdesk_state', (
             ('machine_name', self.gf('django.db.models.fields.CharField')(max_length=32, primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=64)),
             ('resolved', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('color', self.gf('django.db.models.fields.CharField')(default='default', max_length=32)),
         ))
         db.send_create_signal(u'helpdesk', ['State'])
 
         # Adding model 'Ticket'
         db.create_table(u'helpdesk_ticket', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('body', self.gf('django.db.models.fields.TextField')()),
-            ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['helpdesk.Project'])),
-            ('state', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['helpdesk.State'])),
+            ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['helpdesk.Project'], null=True, blank=True)),
+            ('state', self.gf('django.db.models.fields.related.ForeignKey')(default='open', to=orm['helpdesk.State'])),
             ('priority', self.gf('django.db.models.fields.IntegerField')(default=1)),
             ('assignee', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
             ('customer', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('message_id', self.gf('django.db.models.fields.CharField')(unique=True, max_length=256)),
+            ('message_id', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
         db.send_create_signal(u'helpdesk', ['Ticket'])
+
+        # Adding model 'Comment'
+        db.create_table(u'helpdesk_comment', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('ticket', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['helpdesk.Ticket'])),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='helpdesk_answers', null=True, to=orm['auth.User'])),
+            ('internal', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('notified', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('message_id', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'helpdesk', ['Comment'])
+
+        # Adding model 'HistoryAction'
+        db.create_table(u'helpdesk_historyaction', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('ticket', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['helpdesk.Ticket'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('change', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'helpdesk', ['HistoryAction'])
 
 
     def backwards(self, orm):
         # Deleting model 'Project'
         db.delete_table(u'helpdesk_project')
 
+        # Deleting model 'HelpdeskProfile'
+        db.delete_table(u'helpdesk_helpdeskprofile')
+
         # Deleting model 'State'
         db.delete_table(u'helpdesk_state')
 
         # Deleting model 'Ticket'
         db.delete_table(u'helpdesk_ticket')
+
+        # Deleting model 'Comment'
+        db.delete_table(u'helpdesk_comment')
+
+        # Deleting model 'HistoryAction'
+        db.delete_table(u'helpdesk_historyaction')
 
 
     models = {
@@ -88,13 +131,41 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
+        u'helpdesk.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'helpdesk_answers'", 'null': 'True', 'to': u"orm['auth.User']"}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'internal': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'message_id': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'notified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ticket': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['helpdesk.Ticket']"})
+        },
+        u'helpdesk.helpdeskprofile': {
+            'Meta': {'object_name': 'HelpdeskProfile'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'signature': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
+        u'helpdesk.historyaction': {
+            'Meta': {'object_name': 'HistoryAction'},
+            'change': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ticket': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['helpdesk.Ticket']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
         u'helpdesk.project': {
             'Meta': {'object_name': 'Project'},
+            'default_assignee': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             'machine_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         u'helpdesk.state': {
             'Meta': {'object_name': 'State'},
+            'color': ('django.db.models.fields.CharField', [], {'default': "'default'", 'max_length': '32'}),
             'machine_name': ('django.db.models.fields.CharField', [], {'max_length': '32', 'primary_key': 'True'}),
             'resolved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '64'})
@@ -106,11 +177,11 @@ class Migration(SchemaMigration):
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'customer': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'message_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256'}),
+            'message_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'priority': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
-            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['helpdesk.Project']"}),
-            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['helpdesk.State']"}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['helpdesk.Project']", 'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'default': "'open'", 'to': u"orm['helpdesk.State']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
         }
     }
