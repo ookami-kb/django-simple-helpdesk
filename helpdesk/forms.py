941d67953c0b97742e34857ea936cfbb3105656f
ookami.kb <ookami.kb@gmail.com>
2015-06-24 01:06:12 +0500

    fixed assignee filter

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -72,7 +72,7 @@ class FilterForm(forms.Form):
             self.fields['email'] = forms.EmailField(required=False)
 
         if view_assignees:
-            choices = User.objects.filter(ticket__isnull=False).annotate(
+            choices = User.objects.filter(ticket__isnull=False, ticket__state='open').annotate(
                 tickets=Count('ticket')).order_by('-tickets')
             assignees = self.ASSIGNEES + tuple(
                 (u.pk, u'{} - {}'.format(self._get_user_label(u), u.tickets)) for u in choices
