da488bb53f09da3fbc75049922d95c15f1e30e47
avalas <aastapp@gmail.com>
2016-05-11 01:06:11 +0800

    Migrations moved to one file

--- helpdesk/migrations/0002_auto_20160510_1703.py
+++ helpdesk/migrations/0002_auto_20160510_1703.py
@@ -17,14 +17,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AttachmentFile',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('file_id', models.CharField(unique=True, max_length=255, default=uuid.uuid1, editable=False)),
                 ('attachment_file', models.FileField()),
             ],
         ),
+
         migrations.AlterField(
             model_name='comment',
             name='message_id',
-            field=models.CharField(max_length=255, unique=True, editable=False, default=uuid.uuid1),
+            field=models.CharField(unique=True, max_length=255, default=uuid.uuid1, editable=False),
+        ),
+        migrations.AlterField(
+            model_name='comment',
+            name='ticket',
+            field=models.ForeignKey(to='helpdesk.Ticket', related_name='comments'),
         ),
         migrations.AlterField(
             model_name='mailattachment',
